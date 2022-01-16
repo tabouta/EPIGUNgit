0e1009abf94423131ac5bc57a7cac13ab1df6358
luckner972 <90464205+luckner972@users.noreply.github.com>
2022-01-17 00:42:38 +0100

    Add files via upload

---
+++ AI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts
+{
+
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AI : MonoBehaviour
+    {
+
+        public int sante = 100;
+
+        public NavMeshAgent agent;
+        public GameObject player;
+        public LayerMask whatIsGround, whatIsPlayer;
+
+        //Patroling
+        public Vector3 walkPoint;
+        bool walkPointSet;
+        public float walkPointRange;
+
+        //Attacking
+        public float timeBetweenAttacks;
+        bool AlreadyAttacked;
+
+        //States
+        public float sightRange, attackRange;
+        public bool playerInSightRange, playerInAttackRange;
+
+        public bool attack = false;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if (player == null)
+                return;
+
+            playerInSightRange = Vector3.Distance(player.transform.position, transform.position) < sightRange;
+            playerInAttackRange = Vector3.Distance(player.transform.position, transform.position) < attackRange;
+
+            if (!playerInSightRange && !playerInSightRange) Patroling();
+            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+            if (playerInSightRange && playerInAttackRange) AttackPlayer();
+        }
+
+        private void Patroling()
+        {
+            if (!walkPointSet) SearchWalkPoint();
+
+            if (walkPointSet)
+                agent.SetDestination(walkPoint);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+            //Walkpoint reached
+            if (distanceToWalkPoint.magnitude < 1f)
+                walkPointSet = false;
+        }
+
+        private void SearchWalkPoint()
+        {
+            float randomZ = Random.Range(-walkPointRange, walkPointRange);
+            float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+            walkPointSet = true;
+        }
+        private void ChasePlayer()
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        private void AttackPlayer()
+        {
+            attack = true;
+            agent.SetDestination(transform.position);
+
+            transform.LookAt(player.transform);
+            if (!AlreadyAttacked)
+            {
+                //Put attack code here
+
+                AlreadyAttacked = true;
+                Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            }
+        }
+
+        private void ResetAttack()
+        {
+            AlreadyAttacked = false;
+        }
+
+        private bool TakeDamages(int d)
+        {
+            sante -= d;
+            if (sante <= 0)
+            {
+                GameObject.Destroy(gameObject);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ AIRespawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class AIRespawn : MonoBehaviour
+    {
+        private int numberOfAI;
+
+        [SerializeField]
+        private int AIincrement;
+
+        [SerializeField]
+        private GameObject AI;
+
+        [SerializeField]
+        private string AITag = "AI";
+
+        [SerializeField]
+        private Vector3 spawnPoint;
+
+        // Use this for initialization
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GameObject.FindGameObjectWithTag(AITag) == null)
+                NewWave();
+        }
+
+        private void NewWave()
+        {
+            GameObject t;
+            numberOfAI += AIincrement;
+            for(int i = 0; i < numberOfAI; i++)
+            {
+                t = Instantiate(AI);
+                t.gameObject.transform.SetPositionAndRotation(spawnPoint,Quaternion.identity);
+            }
+        }
+    }
+}
