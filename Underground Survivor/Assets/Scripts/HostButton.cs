f470f39cdbc41a1f923a91bdbb09618f66d55a0b
KentyVI <arbautquentin@gmail.com>
2022-01-20 11:10:51 +0100

    debug
    tout est ok manque juste les maps et l'ia prendre CETTE VERSION ET PAS LES AUTRRES !

---
+++ Underground Survivor/Assets/Scripts/HostButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class HostConnect : MonoBehaviour
+{
+    NetworkManager manager;
+    public InputField ip_InputField;
+    public GameObject HostConnect_go;
+
+    void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
+
+    public void HostFunction()
+    {
+        manager.StartHost();
+        HostConnect_go.SetActive(false);
+    }
+
+    public void ConnectFunction()
+    {
+        manager.networkAddress = ip_InputField.text;
+        manager.StartClient();
+
+        HostConnect_go.SetActive(false);
+    }
+}
